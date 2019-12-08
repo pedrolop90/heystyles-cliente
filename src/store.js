efdbc852a18f9abcf7e9c7b775d0791097ffd8a4
pedrolop900 <pedro.lopez@orionsoftware.com.co>
2019-12-08 08:40:23 -0500

    cambiadas urls de los micros.

--- src/store.js
+++ src/store.js
@@ -6,10 +6,10 @@ import axios from 'axios'
 Vue.use(Vuex)
 export default new Vuex.Store({
     state: {
-        servidorSeguridad: 'http://ec2-54-196-82-212.compute-1.amazonaws.com:9000/seguridad/',
-        servidorAcceso: 'http://ec2-54-196-82-212.compute-1.amazonaws.com:9010/',
-        servidorProducto: 'http://ec2-54-196-82-212.compute-1.amazonaws.com:9030/',
-        servidorFactura: 'http://ec2-54-196-82-212.compute-1.amazonaws.com:9040/',
+        servidorSeguridad: 'http://ec2-54-236-86-37.compute-1.amazonaws.com:9000/seguridad/',
+        servidorAcceso: 'http://ec2-54-236-86-37.compute-1.amazonaws.com:9010/',
+        servidorProducto: 'http://ec2-54-236-86-37.compute-1.amazonaws.com:9030/',
+        servidorFactura: 'http://ec2-54-236-86-37.compute-1.amazonaws.com:9040/',
         menu: undefined,
         usuarios: [
             {
